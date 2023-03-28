db8e8544b13708c9129f09b2ebbc00899645d246
Gavri Kepets <gkepets@gmail.com>
2023-03-28 17:30:20 -0400

    changed hosty

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
